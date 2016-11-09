a9a082fd94b57681b0efbcb03d2a4a893a242247
geancanova <geancanovawt@gmail.com>
2016-11-09 14:26:32 -0200

    add analytics event tracking js

---
+++ recipes/analytics/ga-event-tracking.js
@@ -0,0 +1,10 @@
+// GA Event Tracker
+ga('send', 'event', [eventCategory], [eventAction], [eventLabel], [eventValue], [fieldsObject]);
+
+
+// Exemplo de aplicação em evento de click
+$('.js-chat-btn').on('click', function (e) {
+    e.preventDefault;
+
+    ga('send', 'event', 'Contato', 'Clique no botão', 'Chat');
+});
